f0dbbfb1a4407cf66ccc5c6f375ef2967eac4790
John L. Armstrong IV <jlarmstrongiv@gmail.com>
2020-12-05 01:45:37 -0500

    feat: 🎸 initial commit

---
+++ src/pages/api/identify.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { PrepareLobe, isLambda, LobeModel } from "tfjs-node-lambda-helpers";
+
+const baseUrl = isLambda()
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:3000`;
+const prepareLobe = PrepareLobe(`${baseUrl}/static/model`);
+
+let model: LobeModel;
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  const lobe = await prepareLobe.next();
+  if (!lobe.done) {
+    return res.status(lobe.value.statusCode).json(lobe.value);
+  } else {
+    model ?? (model = lobe.value);
+  }
+  const imageUrl = req.body.imageUrl;
+
+  const response = await axios.get(imageUrl, {
+    responseType: "arraybuffer",
+  });
+  const results = model.predict(response.data);
+
+  return res.status(200).json({ results: results.Confidences });
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+
+const DEFAULT_ATTEMPTS = 10;
+
+export default function Home() {
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+  const [results, setResults] = React.useState("");
+
+  const identityImage = async (imageUrl, attempts = DEFAULT_ATTEMPTS) => {
+    if (attempts < 0) {
+      setError(`${DEFAULT_ATTEMPTS} attempts failed`);
+      setLoading(false);
+    }
+    try {
+      setResults("");
+      setError("");
+      setLoading(true);
+      const response = await axios.post(`/api/identify`, {
+        imageUrl,
+      });
+      setLoading(false);
+      return response.data.results;
+    } catch (error) {
+      return identityImage(imageUrl, attempts - 1);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          <a
+            href="/static/images/duck.jpg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rubber duck
+          </a>{" "}
+          or a <br />
+          <a
+            href="/static/images/rock.jpg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            bunch of rocks
+          </a>
+          ?
+        </h1>
+
+        <p className={styles.description}>
+          Check out the{" "}
+          <a
+            href="https://www.npmjs.com/package/tfjs-node-lambda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <code className={styles.code}>tfjs-node-lambda</code>
+          </a>{" "}
+          npm package.
+        </p>
+
+        <div className={styles.card}>
+          <input
+            type="text"
+            placeholder="Image URL"
+            onChange={(event) => setImageUrl(event.target.value)}
+            value={imageUrl}
+          />
+          <button
+            onClick={() => {
+              identityImage(imageUrl).then((results) => setResults(results));
+            }}
+          >
+            Identify
+          </button>
+        </div>
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt=""
+            style={{ width: "200px", height: "200px", objectFit: "contain" }}
+          />
+        )}
+        {results && (
+          <div className={styles.card}>
+            <p className={styles.description}>
+              {results["rubber-duck"] > results["rock"]
+                ? "Rubber duck"
+                : "Rock"}
+            </p>
+            <pre>{JSON.stringify(results, null, 2)}</pre>
+          </div>
+        )}
+        {loading && <div className={styles.card}>Loading…</div>}
+        {error && <div className={styles.card}>{error}</div>}
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.npmjs.com/package/tfjs-node-lambda"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <strong style={{ paddingLeft: "4px" }}>Taco 🌮 Tuesday</strong>
+        </a>
+      </footer>
+    </div>
+  );
+}
